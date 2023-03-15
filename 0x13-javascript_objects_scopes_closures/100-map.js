59afdf6d649b1734b9688b7baf0a667c9eeff5f7
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-03-15 15:23:27 +0000

    Advance 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const data = require('./100-data');
+
+const newArray = data.map((value, index) => value * index);
+
+console.log('Initial list:', data);
+console.log('New list:', newArray);
