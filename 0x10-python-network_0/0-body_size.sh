f79098434f68f56c5cb5ddceb6ab0bf3d29b4e8a
straystringz <victor.j.emmanuel@hotmail.com>
2023-04-04 13:46:35 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument was provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the specified URL using cURL and display the size of the response body
-curl -s -w '%{size_download}' -o /dev/null "$1"
+# Takes in a url and sends a request to that url and displays the size of the body of the response
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
