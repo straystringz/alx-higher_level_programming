22a9def5d42e1ac5fd7527f9f583b14350a33420
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-04-25 20:13:01 +0000

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,7 +5,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, res, body) => {
+request(url,  (err, res, body) => {
   if (err) {
     console.error(err);
     return;
