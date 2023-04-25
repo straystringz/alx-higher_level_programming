806ee7a0378a8cd0c69c9c08836c697f65fe0a4a
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-04-25 19:45:03 +0000

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
