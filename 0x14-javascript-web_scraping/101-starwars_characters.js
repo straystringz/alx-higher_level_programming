dc66f2881f565b5d50eb1beca74b9cf25ce8ba01
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-04-25 20:31:06 +0000

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters, 0);
 });
 
-function printCharacters(characters, index) {
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log(JSON.parse(body).name);
-    if (index + 1 < characters.length) {
-      printCharacters(characters, index + 1);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
   });
 }
